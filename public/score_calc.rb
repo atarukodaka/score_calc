d2b61eff9f58fee222f38a6140d05d496718717a
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-12-01 17:38:28 +0000

    sov added

--- public/score_calc.rb
+++ public/score_calc.rb
@@ -8,15 +8,20 @@ class ScoreCalc
       "novalue": OpenStruct.new({name: "novalue", type: :novalue, bv: 0, v: 0, v1: 0, dg: "n/a", sov: [0, 0, 0, 0, 0, 0, 0]}),
 
       ## Jump
-      "3A": OpenStruct.new({name: "3A", type: :jump, bv: 8.5, v: 5.9, v1: 0, dg: "2A", sov: [-3, -2, -1, 0, 1, 2, 3]}),
-      "3Lz": OpenStruct.new({name: "3Lz", type: :jump, bv: 6.0, v: 4.2, v1: 3.6, dg: "2Lz", sov:  [-2.1, -1.4, -0.7, 0, 0.7, 1.4, 2.1]}),
-      "2Lz": OpenStruct.new({name: "2Lz",  type: :jump, bv: 2.1, v: 1.5, v1: 1.4, dg: "1Lz", sov: [-0.9, -0.6, -0.3, 0, 0.3, 0.6, 0.9]}),
-      "3Lo": OpenStruct.new({name: "3Lo",  type: :jump, bv: 5.1, v: 3.6, dg: "2T", sov: [-2.1, -1.4, -0.7, 0, 0.7, 1.4, 2.1]}),
-      "3T": OpenStruct.new({name: "3T",  type: :jump, bv: 4.3, v: 3.0, dg: "2T", sov: [-2.1, -1.4, -0.7, 0, 0.7, 1.4, 2.1]}),
-
+      "3A": OpenStruct.new({name: "3A", type: :jump, bv: 8.5, v: 5.9, v1: 0, 
+                             dg: "2A", sov: [1,1,1,1,1,1,1]}),
+      "3Lz": OpenStruct.new({name: "3Lz", type: :jump, bv: 6.0, v: 4.2, v1: 3.6, 
+                              dg: "2Lz", sov: [1,1,1,1,1,1,1]}),
+      "2Lz": OpenStruct.new({name: "2Lz",  type: :jump, bv: 2.1, v: 1.5, v1: 1.4, 
+                              dg: "1Lz", sov: [1,1,1,1,1,1,1]}),
+      "3Lo": OpenStruct.new({name: "3Lo",  type: :jump, bv: 5.1, v: 3.6, 
+                              dg: "2T", sov: [-2.1, -1.4, -0.7, 0, 0.7, 1.4, 2.1]}),
+      "3T": OpenStruct.new({name: "3T",  type: :jump, bv: 4.3, v: 3.0, 
+                             dg: "2T", sov: [-2.1, -1.4, -0.7, 0, 0.7, 1.4, 2.1]}),
+      
       ## Spin
       "USp1": OpenStruct.new({name: "USp1", type: :spin, bv: 1.7, v: 1.2, sov: [-0.9, -0.6, -0.3, 0, 0.5, 1.0, 1.5]}),
-
+      
       ## Step
       "StSq1": OpenStruct.new({name: "StSq1", type: :stsq, bv: 1.8, sov: [-0.9, -0.6, -0.3, 0, 0.5, 1.0, 1.5]}),
       "StSq2": OpenStruct.new({name: "StSq2", type: :stsq, bv: 2.6, sov: [-1.5, -1.0, -0.5, 0, 0.5, 1.0, 1.5]}),
---
+++ public/sov.rb
@@ -0,0 +1,22 @@
+
+require 'csv'
+
+def run
+  csv_fname = "sov.csv"
+  table = CSV.table(csv_fname)
+
+
+  elems = table.map{|row|
+    sov_str = %Q[{"-3": #{row[:neg3]}, "-2": #{row[:neg2]}, "-1": #{row[:neg1]}, "0": #{row[:flat]}, "1": #{row[:pos1]}, "2": #{row[:pos2]}, "3": #{row[:pos3]}}]
+    %Q["#{row[:element]}": {bv: "#{row[:bv]}", v: "#{row[:v]}", v1: "#{row[:v1]}", sov: #{sov_str}}]
+  }.join(",\n")
+
+  output = "var elements = {
+#{elems}
+}"
+
+  return output
+end
+
+puts run()
+
